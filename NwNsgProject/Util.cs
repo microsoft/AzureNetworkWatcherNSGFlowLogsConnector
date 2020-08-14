4e2e3c9db208b41a6ddca06d715686c34d148c34
Chris Montgomery <cmontgomery2@plex.com>
2020-08-14 10:33:07 -0700

    Updated nuget packages, migrated to Asp.Net Core 3.0 and Functions v3.

--- NwNsgProject/Util.cs
+++ NwNsgProject/Util.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
-using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.Azure.Storage.Blob;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
